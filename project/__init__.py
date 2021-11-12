1596bddb8c8f7d7718fb350c7c67a19fe29db747
yarickprih <yarick.prih2903@hotmail.com>
2021-11-12 15:40:53 +0200

    Add type hints and docstrings to project/__init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -12,14 +12,27 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 
-def create_app():
+def create_app() -> Flask:
+    """Flask app factory
+
+    Returns:
+        Flask: initialized Flask application with all extentions connected and config set
+    """
     app = Flask(__name__)
     app.config.from_object(config_map[os.getenv("CONFIG", "dev")])
     initialize_extensions(app)
     return app
 
 
-def initialize_extensions(app):
+def initialize_extensions(app: Flask) -> Flask:
+    """This function initializes extentions
+
+    Args:
+        app (Flask): Flask app instance
+
+    Returns:
+        Flask: Flask app instance with initialized extentions
+    """
     db.init_app(app)
     ma.init_app(app)
     migrate.init_app(app, db)
