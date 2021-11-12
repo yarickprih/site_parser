1c2c2833300266baaa90fc35ad8178062ab53a02
yarickprih <yarick.prih2903@hotmail.com>
2021-11-12 13:33:19 +0200

    Separate database URL generation logic from Config class
    Implement create_db_url function to generate database URL separately from Config class.

--- project/config.py
+++ project/config.py
@@ -4,10 +4,19 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent
 
 
+def create_db_url() -> str:
+    db_user = os.getenv("POSTGRES_USER")
+    db_password = os.getenv("POSTGRES_PASSWORD")
+    db_host = os.getenv("POSTGRES_HOST")
+    db_port = os.getenv("POSTGRES_PORT")
+    db_name = os.getenv("POSTGRES_DB")
+    return f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+
 class Config:
     DEBUG = bool(os.getenv("DEBUG", "0"))
     SECRET_KEY = str(os.getenv("SECRET_KEY", "secret_key"))
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+    SQLALCHEMY_DATABASE_URI = create_db_url()
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
